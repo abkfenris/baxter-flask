2b55ba8e93702db7082e72066580af20a43bbead
Alex Kerney <abk@mac.com>
2014-12-07 11:30:41 -0500

    Form overrides and arguments for AvalancheIn(cident)

--- baxter/admin/__init__.py
+++ baxter/admin/__init__.py
@@ -126,6 +126,90 @@ class TrailView(ModelView):
 
 class AvalancheInView(ModelView):
     inline_models = (AvalancheInvolved,)
+    form_overrides = dict(aspect=SelectField, 
+                          trigger=SelectField,
+                          trigger_add=SelectField,
+                          av_problem=SelectField,
+                          av_type=SelectField,
+                          weak_layer=SelectField)
+    form_args = dict(
+        aspect= dict(
+			choices=[('unknown', 'Unknown'),
+			         ('N', 'North'),
+					 ('NE', 'North East'),
+					 ('E', 'East'),
+					 ('SE', 'South East'),
+					 ('S', 'South'),
+					 ('SW', 'South West'),
+					 ('W', 'West'),
+					 ('NW', 'North West')
+			]
+			
+		),
+		trigger= dict(
+    		choices=[('unknown', 'Unknown'),
+    		         ('natural', 'Natural'),
+    		         ('skier', 'Skier'),
+    		         ('snowboarder', 'Snowboarder'),
+    		         ('snowmobiler', 'Snowmobiler'),
+                     ('snowbike', 'Snow Bike'),
+                     ('snowshoer', 'Snowshoer'),
+                     ('explosive', 'Explosive'),
+                     ('hiker', 'Hiker'),
+                     ('other', 'Other')
+    		]
+		),
+		trigger_add= dict(
+    		choices=[('unknown', 'Unknown'),
+    		         ('intentional', 'Intentionally Triggered'),
+                     ('unintentional', 'Unintentionally Triggered'),
+                     ('remote', 'Remotely Triggered'),
+                     ('cornice', 'Cornice Triggered'),
+                     ('sympathetic', 'Sympathetic Release'),
+                     ('repeater', 'Repeater')
+    		]
+		),
+		av_problem= dict(
+    		choices=[('unknown', 'Unknown'),
+    		         ('storm', 'Storm Slab'),
+                     ('persistent', 'Persistant Slab'),
+                     ('deep', 'Deep Slab'),
+                     ('wet', 'Wet Slab'),
+                     ('loosedry', 'Loose Dry Snow'),
+                     ('loosewet', 'Loose Wet Snow'),
+                     ('wind', 'Wind Slab'),
+                     ('cornice', 'Cornice'),
+                     ('glide', 'Glide')
+            ]
+		),
+		av_type= dict(
+    		choices=[('U', 'Unknown'),
+    		         ('L', 'Dry Loose'),
+                     ('SS', 'Soft Slab'),
+                     ('HS', 'Hard Slab'),
+                     ('WL', 'Wet Loose'),
+                     ('WS', 'Wet Slab'),
+                     ('C', 'Cornice Fall'),
+                     ('I', 'Ice fall'),
+                     ('SF', 'Slush flow'),
+                     ('R', 'Roof Avalanche'),
+                     
+    		]
+		),
+		weak_layer= dict(
+    		choices=[('unknown', 'Unknown'),
+    		         ('new', 'New Snow'),
+                     ('density', 'Density Change'),
+                     ('interface', 'New Snow/Old Snow Interface'),
+                     ('facets', 'Facets'),
+                     ('depth', 'Depth Hoar'),
+                     ('surface', 'Surface Hoar'),
+                     ('graupel', 'Graupel'),
+                     ('wetgrains', 'Wet Grains'),
+                     ('ground', 'Ground Interface')
+    		]
+		)
+    )
 	
 
 
