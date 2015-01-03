f28bea2a7108afa99dffdb08dafd53f0d2d15913
Alex Kerney <abk@mac.com>
2015-01-02 22:14:30 -0500

    Geojson of multiple avalanche incidents via unions of bed_surface and debris_field, and more data about individual incidents

--- baxter/api_1_0/avalanche_incidents.py
+++ baxter/api_1_0/avalanche_incidents.py
@@ -10,6 +10,7 @@ from .. import db
 from .. import cache
 from ..models import AvalancheIn
 
+
 @api.route('/avalanche/incident/<int:id>/')
 def incident(id):
     """
@@ -17,24 +18,102 @@ def incident(id):
     """
     query = db.session.query(AvalancheIn.name,
             AvalancheIn.id,
+            AvalancheIn.observation_date,
+            AvalancheIn.occurence_date,
+            AvalancheIn.location,
+            AvalancheIn.elevation,
+            AvalancheIn.aspect,
+            AvalancheIn.trigger,
+            AvalancheIn.trigger_add,
+            AvalancheIn.av_problem,
+            AvalancheIn.av_type,
+            AvalancheIn.weak_layer,
+            AvalancheIn.depth,
+            AvalancheIn.width,
+            AvalancheIn.vertical,
+            AvalancheIn.slope_angle,
+            AvalancheIn.description,
             AvalancheIn.crown.ST_Transform(4326).ST_AsGeoJSON().label('crown'),
             AvalancheIn.bed_surface.ST_Transform(4326).ST_AsGeoJSON().label('bed_surface'),
             AvalancheIn.debris_field.ST_Transform(4326).ST_AsGeoJSON().label('debris_field'),
+            AvalancheIn.bed_surface.ST_Union(AvalancheIn.debris_field).ST_Transform(4326).ST_AsGeoJSON().label('geojson')
             ).filter_by(id=id)
 
     incident = query[0]
     return jsonify({
-        'type': 'Feature',
-        'properties': {
-            'name': incident.name,
-            'id': incident.id
-        },
-        'geometry': {
-            'type': 'GeometryCollection',
-            'geometries': [
-                json.loads(incident.crown),
-                json.loads(incident.bed_surface),
-                json.loads(incident.debris_field)
-            ]
-        }
+        'type': 'FeatureCollection',
+        'features': [
+            {
+                'type': 'Feature',
+                'properties': {
+                    'structure': 'crown'
+                },
+                'geometry': json.loads(incident.crown)
+            },
+            {
+                'type': 'Feature',
+                'properties': {
+                    'structure': 'bed surface'
+                },
+                'geometry': json.loads(incident.bed_surface)
+            },
+            {
+                'type': 'Feature',
+                'properties': {
+                    'structure': 'debris field'
+                },
+                'geometry': json.loads(incident.debris_field)
+            },
+            {
+                'type': 'Feature',
+                'properties': {
+                    'name': incident.name,
+                    'id': incident.id,
+                    'observation date': incident.observation_date,
+                    'occurence date': incident.occurence_date,
+                    'location': incident.location,
+                    'elevation': incident.elevation,
+                    'aspect': incident.aspect,
+                    'trigger': incident.trigger,
+                    'trigger info': incident.trigger_add,
+                    'avalanche problem': incident.av_problem,
+                    'avalanche type': incident.av_type,
+                    'weak layer': incident.weak_layer,
+                    'depth': incident.depth,
+                    'width': incident.width,
+                    'vertical': incident.vertical,
+                    'slope angle': incident.slope_angle,
+                    'description': incident.description
+                },
+                'geometry': json.loads(incident.geojson)
+            }
+        ]
+    })
+
+
+@api.route('/avalanche/incident/')
+def incidents():
+    """
+    Geojson for all avalanche incidents
+    """
+    query = db.session.query(AvalancheIn.id,
+            AvalancheIn.occurence_date,
+            AvalancheIn.name,
+            AvalancheIn.bed_surface.ST_Union(AvalancheIn.debris_field).ST_Transform(4326).ST_AsGeoJSON().label('geojson')
+            )
+    incidents = []
+    for incident in query:
+        incidents.append({
+            'type': 'Feature',
+            'properties': {
+                'name': incident.name,
+                'id': incident.id,
+                'occurance date': incident.occurence_date,
+                'url': url_for('.incident', id=incident.id)
+            },
+            'geometry': json.loads(incident.geojson)
+        })
+    return jsonify({
+        'type': 'FeatureCollection',
+        'features': incidents
     })
