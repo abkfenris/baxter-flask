ddb0049bc5abcaa1c6d20a64e85745b3e93a8ff7
Alex Kerney <abk@mac.com>
2015-02-07 08:54:18 -0500

    Error pages #49

--- baxter/main/errors.py
+++ baxter/main/errors.py
@@ -16,7 +16,7 @@ def forbidden(e):
         response = jsonify({'error': 'forbidden'})
         response.status_code = 403
         return response
-    return render_template('403.html'), 403
+    return render_template('errors/403.html'), 403
 
 
 @main.app_errorhandler(404)
@@ -30,7 +30,7 @@ def page_not_found(e):
         response = jsonify({'error': 'not found'})
         response.status_code = 404
         return response
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 
 @main.app_errorhandler(500)
@@ -44,4 +44,4 @@ def internal_server_error(e):
         response = jsonify({'error': 'internal server error'})
         response.status_code = 500
         return response
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
